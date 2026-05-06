d79b9a81794d044b885fb22db7ef621c58df16d7
mickeyyo55b <mickeyyo55b@gmail.com>
2026-05-06 15:29:53 +0800

    05061529

--- BradJava/src/tw/brad/tutor/GuessNumber.java
+++ BradJava/src/tw/brad/tutor/GuessNumber.java
@@ -1,6 +1,8 @@
 package tw.brad.tutor;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -23,7 +25,10 @@ public class GuessNumber extends JFrame implements ActionListener{
 		
 		guess = new JButton("猜");
 		input = new JTextField();
+		input.setFont(new Font(null, Font.BOLD + Font.ITALIC, 24));	// 1, 2, 4
 		log = new JTextArea();
+		log.setFont(new Font(null, Font.BOLD + Font.ITALIC, 24));
+		log.setForeground(Color.GREEN);
 		
 		guess.addActionListener(this);
 //		guess.addActionListener(new ActionListener() {
@@ -83,8 +88,9 @@ public class GuessNumber extends JFrame implements ActionListener{
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		counter++;
 		String g = input.getText();
+		counter++;
+		
 		String result = checkAB(g);
 		log.append(String.format("%d. %s => %s\n", counter, g, result));
 		input.setText("");
