a15d2b4dffdfab5215950ec18288584092a2077c
mickeyyo55b <mickeyyo55b@gmail.com>
2026-05-12 11:11:00 +0800

    05121110

--- BradJava/src/tw/brad/tutor/Brad61.java
+++ BradJava/src/tw/brad/tutor/Brad61.java
@@ -11,6 +11,13 @@ public class Brad61 {
 			DatagramPacket packet = new DatagramPacket(buf, buf.length);
 			socket.receive(packet);
 			System.out.println("UDP Receive OK");
+			
+			String urIp = packet.getAddress().getHostAddress();
+			byte[] data = packet.getData();
+			int len = packet.getLength();
+			System.out.printf("%s : %s\n", urIp, new String(data,0,len));
+			
+			
 		}catch(Exception e) {
 			System.out.println(e);
 		}
